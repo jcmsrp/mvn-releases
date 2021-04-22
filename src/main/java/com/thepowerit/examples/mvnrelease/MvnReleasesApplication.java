b4d84e7f043aedb241a67ef1a914de42ad758008
Julio César Mendoza <jc.msrp@gmail.com>
2021-04-22 18:32:20 +0200

    initial commit

---
+++ src/main/java/com/thepowerit/examples/mvnrelease/MvnReleasesApplication.java
@@ -0,0 +1,13 @@
+package com.thepowerit.examples.mvnrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnReleasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnReleasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thepowerit/examples/mvnrelease/MvnReleasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thepowerit.examples.mvnrelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnReleasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
